457fdd515d696d8d5c23e4c90869a62fca5df66e
sanpedrozz <sup686@gmail.com>
2022-01-21 16:48:01 +0300

    uncomment main.py

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ from config import Config
 print(1)
 if __name__ == '__main__':
     try:
-        # server = mp.Process(target=web.run_app, args=(app, ), kwargs={'port': 8081})
-        # server.start()
-        # logger.info("Web server started")
+        server = mp.Process(target=web.run_app, args=(app, ), kwargs={'port': 8081})
+        server.start()
+        logger.info("Web server started")
 
-        # for plc in Config.PLCS:
-        #     worker = Worker(**plc)
-        #     worker_process = mp.Process(target=worker.run)
-        #     worker_process.start()
+        for plc in Config.PLCS:
+            worker = Worker(**plc)
+            worker_process = mp.Process(target=worker.run)
+            worker_process.start()
 
         db_worker = DBWorker(Config.PLCS)
-        db_worker.run()
-        # db_worker_process = mp.Process(target=db_worker.run)
-        # db_worker_process.start()
+        # db_worker.run()
+        db_worker_process = mp.Process(target=db_worker.run)
+        db_worker_process.start()
 
     except Exception as error:
         logger.error(error)
